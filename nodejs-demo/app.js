1f98dc6c1c813632576a6b42bed4756206bfcfac
LeonZhou <pfzhou@gmail.com>
2015-04-15 01:31:33 +0800

    完成demo

--- nodejs-demo/app.js
+++ nodejs-demo/app.js
@@ -9,6 +9,17 @@ var routes = require('./routes/index');
 var users = require('./routes/users');
 var ejs = require('ejs');
 
+//var connect = require('connect');
+//var mongoose = require('mongoose');
+//mongoose.connect('mongodb://localhost/test');
+var session = require('express-session');
+// var SessionStore = require('session-mongoose')(session);
+// var store = new SessionStore({
+//   url: "mongodb://localhost/session",
+//   interval: 120000
+//   //connection: mongoose.connection
+// });
+
 var app = express();
 
 // view engine setup
@@ -23,10 +34,37 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+// app.use(express.session({
+//     secret: 'my secret',
+//     store: store,
+//     cookie: { maxAge: 900000 } // expire session in 15 min or 900 seconds
+// }));
+app.use(session({ resave: true,
+                  saveUninitialized: true,
+                  secret: 'my secret' }));
+app.use(function(req, res, next){
+  res.locals.user = req.session.user;
+  var err = req.session.error;
+  delete req.session.error;
+  res.locals.message = '';
+  if (err) res.locals.message = '<div class="alert alert-danger">' + err + '</div>';
+  next();
+});
+
+
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+app.all('/login', routes.notAuthentication);
+app.get('/login', routes.login);
+app.post('/login', routes.doLogin);
+app.get('/logout', routes.authentication);
+app.get('/logout', routes.logout);
+app.get('/home', routes.authentication);
+app.get('/home', routes.home);
+
 app.use('/', routes);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -47,7 +85,9 @@ if (app.get('env') === 'development') {
       error: err
     });
   });
-}
+};
+
+app.get('/users', users);
 
 // production error handler
 // no stacktraces leaked to user
--- nodejs-demo/routes/index.js
+++ nodejs-demo/routes/index.js
@@ -3,7 +3,56 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'index' });
 });
 
+router.login = function(req, res, next){
+	res.render('login', { title: '用户登录'});
+	debugger;
+};
+
+router.doLogin = function(req, res){
+	var user={
+		username:'admin',
+		password:'1'
+	}
+	debugger;
+	if(req.body.username==user.username && req.body.password==user.password){
+		console.log('ready redirect to home');
+		req.session.user = user;
+		return res.redirect('/home');
+	}
+	else{
+		req.session.error = '用户名或密码不正确';
+		return res.redirect('/login');
+	}
+};
+
+router.logout = function(req, res){
+	req.session.user = null;
+	res.redirect('/');
+};
+
+router.home = function(req, res){
+	console.log('in home');
+	res.render('home', { title: 'Home'});
+};
+
+router.authentication = function(req, res, next) {
+	if (!req.session.user) {
+		req.session.error='请先登录';
+		return res.redirect('/login');
+	}
+	next();
+}
+
+router.notAuthentication = function(req, res, next) {
+	if (req.session.user) {
+		req.session.error='已登录';
+		return res.redirect('/home');
+	}
+	next();
+}
+
 module.exports = router;
+
